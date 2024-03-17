1bac1d92945b7ecef18c820f88781be5f87be827
Felix Morgner <felix.morgner@gmail.com>
2024-03-17 09:16:13 +0100

    build: fix platform memory configuration

--- source/cmake/Platforms/ATmega32u4.cmake
+++ source/cmake/Platforms/ATmega32u4.cmake
@@ -1,7 +1,7 @@
 include_guard()
 
 add_compile_definitions(
-    "F_CPU=16000000"
+    "F_CPU=16000000L"
 )
 
 add_compile_options(
@@ -19,4 +19,7 @@ add_compile_options(
 add_link_options(
     "-mmcu=atmega32u4"
     "-Wl,--gc-sections"
+    "-Wl,--defsym=__TEXT_REGION_LENGTH__=28k"
+    "-Wl,--defsym=__DATA_REGION_LENGTH__=2560"
+    "-Wl,--defsym=__EEPROM_REGION_LENGTH__=1k"
 )
